54bff4fada539df855f8a3dd150ec2e55cac10ee
Alain Herve <alain.1337dev@outlook.com>
2023-05-26 05:33:11 -0700

    Delete install_azCli.ps1

--- install_azCli.ps1
+++
@@ -1,9 +0,0 @@
-if ($IsLinux) {
-    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-} elseif ($IsMacOS) {
-    brew update && brew install azure-cli
-} elseif ($IsWindows) {
-    Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
-} else {
-    throw "Unknowsn os"
-}
