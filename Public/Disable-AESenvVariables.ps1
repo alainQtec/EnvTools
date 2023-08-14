bba5deb5523608c6f1a30daedb3ec3761dcbe9b6
Alain Herve <alain.1337dev@outlook.com>
2023-08-14 06:04:52 +0200

    Create Disable-AESenvVariables.ps1

---
+++ Public/Disable-AESenvVariables.ps1
@@ -0,0 +1,33 @@
+﻿function Disable-AESenvVariables {
+    <#
+    .SYNOPSIS
+        Disable AES encrypted Environment Variables
+    .DESCRIPTION
+        Configures/deactivates AES encryption in Environment Variables
+        ie: Once run, all cmdlets of EnvTools PsModule will stop using AES when seting environment variables.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        Specify a URI to a help page, this will show when Get-Help -Online is used.
+    .LINK
+        Initialize-EnvConfig
+    .LINK
+        Disable-AESenvVariables
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+    }
+
+    process {
+    }
+
+    end {
+    }
+}
