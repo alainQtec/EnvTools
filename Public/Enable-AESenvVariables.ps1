67a37824866d5ea92a8e1ec61f70a0de868a20d5
Alain Herve <alain.1337dev@outlook.com>
2023-08-14 06:04:49 +0200

    Create Enable-AESenvVariables.ps1

---
+++ Public/Enable-AESenvVariables.ps1
@@ -0,0 +1,31 @@
+﻿function Enable-AESenvVariables {
+    <#
+    .SYNOPSIS
+        Enable AES encrypted Environment Variables
+    .DESCRIPTION
+        Configures/activates AES encryption in Environment Variables.
+        ie: Once run, all cmdlets of EnvTools PsModule will use AES when seting environment variables.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        Initialize-EnvConfig
+    .LINK
+        Enable-AESenvVariables
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+    }
+
+    process {
+    }
+
+    end {
+    }
+}
